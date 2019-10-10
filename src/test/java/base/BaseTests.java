f5baa92716425e652ee088260a5e085132e7350b
cmsrrent <rob.renton@gmail.com>
2019-10-11 00:45:05 +0100

    Up to Chapter 4.3

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,39 +1,29 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size() + " links on this page");
-
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
-
-        //driver.manage().window().maximize();
-        //driver.manage().window().fullscreen();
-        //driver.manage().window().setSize(new Dimension(375,812));
-
-        System.out.println(driver.getTitle());
-        //driver.quit();
+        homePage = new HomePage(driver);
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
+
 
 }
